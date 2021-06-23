65cc5ea84de7a7c16ff0375a226da0664c9aeae7
Roberto Lozada <robertojoselozada@gmail.com>
2021-06-23 15:12:59 -0500

    urls y views agregadas

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from core.views import *
 urlpatterns = [
-    path('test', home)
+    path('', home, name='home'),
+    path('listar/', ListarCanciones, name='listar'),
+    path('agregar/', CrearCancion, name="agregar"),
+    path('eliminar/<int:id_cancion>/', EliminarCancion, name='eliminar'),
+    path('editar/<int:id_cancion>/', EditarCancion, name='editar'),
+    path('info/<int:id_cancion>/', VerInfor, name='informacion'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,51 @@
 from django.http import response
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
-def home(request):
-    return HttpResponse("ruta vacia")
+from core.models import cancion
+from .forms import *
+def home(request):    
+    return render(request, 'index.html', {})
+
+
+def ListarCanciones(request):
+    canciones = cancion.objects.all()
+    context = {'canciones': canciones}
+    return render(request, 'listarcanciones.html', context)
+
+
+def CrearCancion(request):
+    if request.method == "POST":
+        form = CancionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar')
+    elif request.method == "GET":
+        form = CancionForm()
+    context = {'form': form.as_p}
+    return render(request, 'crearcancion.html', context)
+
+
+def EliminarCancion(request, id_cancion):
+    cancionEliminar = cancion.objects.get(id=id_cancion)
+    cancionEliminar.delete()
+    return redirect('listar')
+
+
+def EditarCancion(request, id_cancion):
+    cancionEditar = cancion.objects.get(id=id_cancion)
+    if request.method == "POST":
+        form = CancionForm(request.POST, instance=cancionEditar)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    elif request.method == "GET":
+        form = CancionForm(instance=cancionEditar)
+    context = {'form': form.as_p}
+    return render(request, 'editarcancion.html', context)
+
+
+def VerInfor(request, id_cancion):
+    cancionInfo = cancion.objects.get(id=id_cancion)
+    context = {'cancion': cancionInfo}
+    return render(request, 'informacion.html', context)
